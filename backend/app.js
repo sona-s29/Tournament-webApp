8a4e225ff7f2074ff3e3a3c55c09fc51284d554e
Sona <sona2529001@gmail.com>
2025-09-18 21:40:47 +0530

    updated README

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,17 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-app.use(cors());
+const cors = require("cors");
+
+app.use(cors({
+  origin: [
+    "https://classytournament.vercel.app/", // your deployed frontend domain
+    "https://tournament-webapp.onrender.com"             // for local dev (Vite default)
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Routes
